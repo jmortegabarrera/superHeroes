b923d5303b3c1b9cc65fbb2c2dc5b76ac6b310df
jmortega <jmortegabarrera@gmail.com>
2021-06-05 08:54:30 +0200

    add 1 test to superheroes service

--- src/app/core/services/super-heroes.service.spec.ts
+++ src/app/core/services/super-heroes.service.spec.ts
@@ -1,16 +1,41 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
 import { SuperHeroesService } from './super-heroes.service';
+import { FilterInput } from '../models/shared/filter.input';
+import { GetAllHeroesResponse } from '../models/superhero/getAllHeroes.response';
+import { SuperHero } from '../models/superhero/superhero.model';
 
-describe('SuperHeroesService', () => {
+fdescribe('SuperHeroesService', () => {
   let service: SuperHeroesService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      providers: [SuperHeroesService]
+    });
     service = TestBed.inject(SuperHeroesService);
+    
   });
-
-  it('should be created', () => {
+  
+  it('should get all heroes', () => {
+    const filter = createFilter()
+    const hero = new SuperHero(1,'1','1','1');
+    const getAllHeroesResponse = new GetAllHeroesResponse(hero)
+    service.getAllHeroes$(filter).subscribe(result => {
+      expect(result).toBe(getAllHeroesResponse)
+    })
+    expect(service).toBeTruthy();
+  });  
+  it('should get all heroes count', () => {
+    service.getAllHeroesCount$().subscribe(result => {
+      expect(result).toBe(0)
+    })
     expect(service).toBeTruthy();
   });
 });
+
+
+function createFilter() {
+  return new FilterInput();
+}
+
